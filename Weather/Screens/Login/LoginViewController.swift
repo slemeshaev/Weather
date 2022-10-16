2da5ef4a8abc8db7be3ea0e27bc85006ea25183a
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-10-16 13:56:49 +0300

    Initial project

---
+++ Weather/Screens/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Weather
+//
+//  Created by Stanislav Lemeshaev on 16.10.2022.
+//  Copyright © 2022 slemeshaev. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
