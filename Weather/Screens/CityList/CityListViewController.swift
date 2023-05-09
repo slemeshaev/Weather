375ad5a9dcfcfc1828044f134aa60e5c97e16f62
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2023-05-09 13:44:16 +0300

    City List View
    - Added CityListView to display cities list

---
+++ Weather/Screens/CityList/CityListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CityListViewController.swift
+//  Weather
+//
+//  Created by Stanislav Lemeshaev on 09.05.2023.
+//  Copyright © 2023 slemeshaev. All rights reserved.
+//
+
+import UIKit
+
+class CityListViewController: UIViewController {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
